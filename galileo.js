d41d23d75a2a70d8e3fd2e35661de8517bfd058e
rtft <u1025269@utah.edu>
2020-10-18 15:10:45 -0600

    FIxed some stuff

--- galileo.js
+++ galileo.js
@@ -1,6 +1,8 @@
 
 var access_token;
 var refresh_token;
+var cardHolder_id;
+var account_id;
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
@@ -121,7 +123,6 @@ function createCardholder(first_name, last_name, email, password, DOB, id_string
       xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             var response = JSON.parse(this.responseText);
-            xhr = null;
             return response.cardholder_id;
         }
       });
@@ -141,14 +142,15 @@ function fundAccount(destinationAccountID, sourceAccountID, amount){
       });
       
       const xhr = new XMLHttpRequest();
-      
+      var accounts;
       xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
-            if (this.status == 201){
-
-            }else{
-                console.log(this.responseText);
-            }
+          for (i in response.accounts){
+            console.log(i)
+            accounts = response.accounts[i];
+          }
+          xhr = null;
+          return accounts;
         }
       });
       
@@ -158,6 +160,24 @@ function fundAccount(destinationAccountID, sourceAccountID, amount){
       xhr.setRequestHeader("Authorization", "Bearer " + access_token);
       
       xhr.send(data);
+}
+
+function listAccounts(cardholderID){
+    const data = null;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      var response = JSON.parse(this.responseText);
+      return 
+    }
+  });
+
+  xhr.open("GET", "https://sandbox.galileo-ft.com/instant/v1/cardholders/cardholder_id/accounts");
+  xhr.setRequestHeader("accept", "*/*");
+
+  xhr.send(data);
 }
 
 function executeTransaction(cardholderID, accountID, merchantName, amount){
@@ -173,11 +193,19 @@ function executeTransaction(cardholderID, accountID, merchantName, amount){
           console.log(this.responseText);
         }
       });
-      
+      jkjk
       xhr.open("POST", "https://sandbox.galileo-ft.com/instant/v1/cardholders/" + cardholderID + "/accounts/" + accountID + "/transactions");
       xhr.setRequestHeader("accept", "*/*");
       xhr.setRequestHeader("content-type", "application/json");
       xhr.setRequestHeader("Authorization", "Bearer " + access_token);
       
       xhr.send(data);
+}
+
+function setupUser(){
+  var dashboardAccountID = 10467;
+  loginToGalileo();
+  cardHolder_id = createCardholder("firstname", "lastname", "test@gmail.com","Password123$%^", "2000-01-01", "111223333", "ssn", "u150k" , "weekly", "science_engineering", "employment", "1204378956", "1234E 100S", "Apt. 1", "Salt Lake City", "UT", "84102", "1234E 100S", "Apt. 1", "Salt Lake City", "UT", "84102", 19467);
+  var accounts = listAccounts(cardHolder_id);
+  fundAccount(accounts[0], dashboardAccountID, 20);
 }
